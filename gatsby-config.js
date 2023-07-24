f8714b736e6bfe257c4a5e7ae855f5cfdb231189
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-07-24 15:01:10 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `react-weather-app`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag"]
+};
